c15f5169bf2000558422a31f9ba22d3c6a134466
yangxcc <1434890515@qq.com>
2021-10-04 19:59:10 +0800

    插入排序

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -15,8 +15,13 @@ public class InsertSort {
             return;
         }
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i - 1; j >= 0 && arr[j+1] < arr[j]; j--) {  // 0到i的数组是有序的了
-                swap(arr,j+1,j);   // 注意交换的数字,在插入排序一定要注意是j+1和j比较，相邻的两个数据比较
+//            for (int j = i - 1; j >= 0 && arr[j+1] < arr[j]; j--) {  // 0到i的数组是有序的了
+//                swap(arr,j+1,j);   // 注意交换的数字,在插入排序一定要注意是j+1和j比较，相邻的两个数据比较
+//            }
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] < arr[j]) {
+                    swap(arr,i,j);
+                }
             }
         }
     }
