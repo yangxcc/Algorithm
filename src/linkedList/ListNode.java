dfc53c3632a84711abb34c9c52689847bf9922e3
yangxcc <1434890515@qq.com>
2021-10-08 17:27:11 +0800

    单链表

--- src/linkedList/ListNode.java
+++ src/linkedList/ListNode.java
@@ -1,10 +1,29 @@
 package linkedList;
 
 /**
- * Node节点*/
-public class Node {
-    int id;
-    public Node(int id) {
-        this.id = id;
+ * Node节点
+ */
+public class ListNode {
+    Integer val;
+    ListNode next;
+    ListNode rand;
+
+    ListNode() {
     }
+
+    ListNode(Integer val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int val, ListNode next, ListNode rand) {
+        this.val = val;
+        this.next = next;
+        this.rand = rand;
+    }
+
 }
