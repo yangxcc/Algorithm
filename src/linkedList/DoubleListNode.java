82abc425bf672bdffbe82dbab593f276a7d30cba
yangxcc <1434890515@qq.com>
2021-10-08 17:25:44 +0800

    双链表

---
+++ src/linkedList/DoubleListNode.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class DoubleListNode {
+    public Integer val;
+    public DoubleListNode pre;
+    public DoubleListNode next;
+
+    public DoubleListNode(Integer val) {
+        this.val = val;
+    }
+}
