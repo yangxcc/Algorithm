224221c59446abdb956ca66f781e494b6e6a0702
yangxcc <1434890515@qq.com>
2021-10-20 20:59:19 +0800

    leetcode98题，判断是否为二叉平衡树

---
+++ leetcode/tree/isBST.java
@@ -0,0 +1,31 @@
+package tree;
+
+import binaryTree.Node;
+
+/**
+ * 98. 验证二叉搜索树
+ */
+public class isBST {
+
+    public boolean isValidBST(Node root) {
+        if (root == null) {
+            return false;
+        }
+        Node min = null;
+        Node max = null;
+        return process(root, min, max);
+    }
+
+    public boolean process(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        return process(root.left, min, root) && process(root.right, root, max);
+    }
+}
