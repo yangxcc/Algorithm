c4b2131ac065e601c4a135faf6de1cb96cb443ea
yangxcc <1434890515@qq.com>
2021-12-10 18:42:08 +0800

    递归和新建链表实现

--- leetcode/剑指offer/offer25.java
+++ leetcode/剑指offer/offer25.java
@@ -21,4 +21,37 @@ public class offer25 {
             return l2;
         }
     }
+
+    // 其实这道题更容易想到的做法还是通过创建一个新的链表
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode res = new ListNode(-1);
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        ListNode cur;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                cur = new ListNode(head1.val);
+                res.next = cur;
+                head1 = head1.next;
+            } else {
+                cur = new ListNode(head2.val);
+                res.next = cur;
+                head2 = head2.next;
+            }
+            res = res.next;
+        }
+        if (head1 != null) {
+            res.next = head1;
+        }
+        if (head2 != null) {
+            res.next = head2;
+        }
+        return res.next;
+    }
 }
